e4a2a3940a62f441f956c3ca66311fd3213fb7ba
alladasaivamsi <109912485+alladasaivamsi@users.noreply.github.com>
2023-02-03 11:38:25 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,25 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(str)
+{
+    str = str.toLowerCase();
+  
+    let arr = [...str];
+    let arrCopy = [..str];
+    let arrReverse = arr.reverse();
+        arr = arrCopy;
+  
+    console.log(`reverseArray:${arrReverse}`);
+    console.log(`array:${arr}`);
 
+    for(let i = 0; i < arr.length; i++) 
+    {
+      if(arrCopy[i] != arrReverse[i]) 
+      {
+        return false;
+      }
+    }
+    return true;
 }
+console.log(palindrome("abc"));
 module.exports = palindrome
